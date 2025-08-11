202408398088758d331b59685d111edae87bd8fc
Inreet Kaur <reetkhehra11@gmail.com>
2025-08-11 11:32:33 -0400

    Added edit and delete modals.

---
+++ customer-directory-frontend/src/components/DeleteConfirmationModal.tsx
@@ -0,0 +1,28 @@
+import "./Modal.css"
+
+interface props {
+    onClose: () => void;
+    onConfirm: () => void;
+}
+
+export default function DeleteConfirmationModal ({onClose, onConfirm}: props) {
+
+    return (
+        <div className="modal" onClick={onClose}>
+            <div className="modalCard" onClick={e => e.stopPropagation()}>
+                <h2 className="heading">Are you sure you want to delete?</h2>
+                <form className="modalForm">
+                    <label className="heading">This action cannot be reversed.</label>
+                    <div className="modalButtons">
+                        <button className="button delete" onClick={onConfirm}>
+                            Confirm
+                        </button>
+                        <button className="button cancel" onClick={onClose}>
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ customer-directory-frontend/src/components/EditCustomerModal.tsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import type Customer from "../types/customer";
+import "./Modal.css"
+
+interface props {
+    customer: Customer
+    onClose: () => void;
+    onSave: (updatedCustomer: Customer) => void;
+}
+
+export default function EditCustomerModal({customer, onClose, onSave }: props) {
+
+    const [formData, setFormData] = useState(customer);
+
+    const handleChange = (e: any) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = () => {
+        const updatedCustomer = formData
+        onSave(updatedCustomer)
+    };
+
+    return (
+        <div className="modal" onClick={onClose}>
+            <div className="modalCard" onClick={e => e.stopPropagation()}>
+                <h2 className="heading">Add a new customer</h2>
+                <form className="modalForm">
+                    <div className="modalGrid">
+                        <label className="label">Name</label>
+                        <input className="modalInput"
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                        <label className="label">Age</label>
+                        <input className="modalInput"
+                            type="number"
+                            name="age"
+                            placeholder="Age"
+                            value={formData.age}
+                            onChange={handleChange}
+                        />
+                        <label className="label">Gender</label>
+                        <input className="modalInput"
+                            type="text"
+                            name="gender"
+                            placeholder="Gender"
+                            value={formData.gender}
+                            onChange={handleChange}
+                        />
+                        <label className="label">Email</label>
+                        <input className="modalInput"
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                        <label className="label">Password</label>
+                        <input className="modalInput"
+                            type="text"
+                            name="password"
+                            placeholder="Password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                        <label className="label">Address</label>
+                        <input className="modalInput"
+                            type="text"
+                            name="address"
+                            placeholder="Address"
+                            value={formData.address}
+                            onChange={handleChange}
+                        />
+                        <label className="label">Image</label>
+                        {/* <input className="modalInput"
+                            type="text"
+                            name="address"
+                            placeholder="Image"
+                            value={formData.address}
+                            onChange={handleChange}
+                        /> */}
+                        {/* replace with upload button */}
+                    </div>
+                    <div className="modalButtons">
+                        <button className="button save" onClick={handleSubmit}>
+                            Save
+                        </button>
+                        <button className="button cancel" onClick={onClose}>
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
