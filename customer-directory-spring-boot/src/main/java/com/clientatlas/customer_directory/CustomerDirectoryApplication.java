99bd9b1c9f53f5f0d3c7228fc19e37b1f03f2851
Cyruss Amante <cyruss_amante@yahoo.ca>
2025-08-21 13:18:24 -0400

    add spring boot project

---
+++ customer-directory-spring-boot/src/main/java/com/clientatlas/customer_directory/CustomerDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.clientatlas.customer_directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDirectoryApplication.class, args);
+	}
+
+}
---
+++ customer-directory-spring-boot/src/test/java/com/clientatlas/customer_directory/CustomerDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientatlas.customer_directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
