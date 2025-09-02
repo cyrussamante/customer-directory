6ddade2e158ccc4d860e8d22256af6518726f0c2
Cyruss Amante <cyruss_amante@yahoo.ca>
2025-09-02 09:28:19 -0400

    remove old backend

--- customer-directory-backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { chat } = require('../controllers/chatController');
-
-router.post('/chat', chat);                   
-router.get('/ping', (req, res) => res.json({ ok: true }));
-
-module.exports = router;
